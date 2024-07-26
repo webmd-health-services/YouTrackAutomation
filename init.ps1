ae989f80d625aa9f9ff25964acfa422a20e067c9
Cameron Williamson <cwilliamson@webmd.net>
2024-07-26 08:59:10 -0700

    Ending Youtrack process on build servers

--- init.ps1
+++ init.ps1
@@ -1,4 +1,4 @@
-using namespace Microsoft.Playwright
+
 <#
 .SYNOPSIS
 Gets your computer ready to develop the YouTrackAutomation module.
@@ -45,14 +45,3 @@ if (-not (Test-Path -Path $packageRoot))
 {
     New-Item -Path $packageRoot -ItemType Directory
 }
-
-Install-Package -Name 'YouTrackSharp' -Destination $packageRoot -Force -RequiredVersion '2022.3.1' -SkipDependencies
-Install-Package -Name 'Newtonsoft.Json' -Destination $packageRoot -Force -RequiredVersion '13.0.1' -SkipDependencies
-
-$binPath = Join-Path -Path $PSScriptRoot -ChildPath 'YouTrackAutomation\bin'
-if (-not (Test-Path -Path $binPath))
-{
-    New-Item -Path $binPath -ItemType Directory
-}
-Copy-Item -Path (Join-Path -Path $packageRoot -ChildPath 'YouTrackSharp.2022.3.1\lib\netstandard2.0\YouTrackSharp.dll') -Destination $binPath -Force
-Copy-Item -Path (Join-Path -Path $packageRoot -ChildPath 'Newtonsoft.Json.13.0.1\lib\netstandard2.0\Newtonsoft.Json.dll') -Destination $binPath -Force
